74f79027dc803f31a35d93061b35c2dcec8476f6
Naveen <naveensmart87@gmail.com>
2017-01-18 13:07:50 +0100

    world function

---
+++ support/world.js
@@ -0,0 +1,30 @@
+/* global browser */
+'use strict';
+
+var _ = require('underscore');
+
+module.exports = function () {
+    this.World = function World() {
+
+        this.clearLocalStorage = function () {
+            browser.executeScript('localStorage.clear();');
+
+        };
+
+        this.flattenTable = function (table) {
+            // Array of arrays e.g.
+            // [ [ 'First todo' ], [ 'Second todo' ] ]
+            table = table.raw();
+
+            // Flatten array
+            // [ 'First todo', 'Second todo' ]
+            return _.flatten(table);
+        };
+
+        //done(); // tell Cucumber we're finished and to use 'this' as the world instance
+
+        
+    };
+
+
+};
